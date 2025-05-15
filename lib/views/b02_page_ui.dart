c22e8a6605eaedf8c164121172c0fc99639d2efc
Tanatorn-hub <jaysiam46@gmail.com>
2025-05-15 23:06:36 +0700

    ิb01 เสร็จ

---
+++ lib/views/b02_page_ui.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class B02PageUI extends StatefulWidget {
+  const B02PageUI({super.key});
+
+  @override
+  State<B02PageUI> createState() => _B02PageUIState();
+}
+
+class _B02PageUIState extends State<B02PageUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
